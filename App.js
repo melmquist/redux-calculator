8e14c3f55ebd7182a1ab9c59cf0cada6dd65b018
Matthew Elmquist <mattelmquist@gmail.com>
2017-10-06 12:06:56 -0400

    project scaffold

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World ReactJS!!!
+            </div>
+        );
+    }
+}
+
+export default App;
